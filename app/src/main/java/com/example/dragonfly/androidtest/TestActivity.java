5da15323754913660d15f9f27d67bf4487479b64
dragonfly <longyuan02@qq.com>
2017-01-12 10:04:04 +0800

    new sharepreperence

--- app/src/main/java/com/example/dragonfly/androidtest/TestActivity.java
+++ app/src/main/java/com/example/dragonfly/androidtest/TestActivity.java
@@ -2,13 +2,17 @@ package com.example.dragonfly.androidtest;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.text.Html;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
@@ -16,6 +20,7 @@ import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -27,6 +32,7 @@ import java.util.List;
 
 public class TestActivity extends Activity {
     static float density = 0;
+    private Button btn_into;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,18 +43,30 @@ public class TestActivity extends Activity {
         density = metric.density;
         Log.i("density==",density+"");
         WebView mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.loadUrl("https://www.baidu.com");
-        mWebView.loadUrl("https://v21m.mazhan.com/recommend/HighRisk.html?src=android");
+        btn_into= (Button) findViewById(R.id.btn_into);
+        btn_into.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(TestActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        mWebView.loadUrl("http://192.168.1.22:8023/userassets/myAssets/userId/24");
+//        mWebView.loadUrl("https://v21m.mazhan.com/recommend/HighRisk.html?src=android");
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
-        mWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);  //设置 缓存模式
+        if(isNetworkConnected(this)){
+            mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);  //设置 缓存模式
+        }else{
+            mWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);  //设置 缓存模式
+        }
         // 开启 DOM storage API 功能
         mWebView.getSettings().setDomStorageEnabled(true);
         //开启 database storage API 功能
         mWebView.getSettings().setDatabaseEnabled(true);
         String cacheDirPath = getFilesDir().getAbsolutePath()+"appcach";
         //      String cacheDirPath = getCacheDir().getAbsolutePath()+Constant.APP_DB_DIRNAME;
-        Log.i("Tag", "cacheDirPath="+cacheDirPath);
+        Log.i("Tag====", "cacheDirPath="+cacheDirPath);
         //设置数据库缓存路径
         mWebView.getSettings().setDatabasePath(cacheDirPath);
         //设置  Application Caches 缓存目录
@@ -92,13 +110,6 @@ public class TestActivity extends Activity {
                 super.onReceivedError(view, errorCode, description, failingUrl);
             }
         });
-//        ImageView imgageView = (ImageView) findViewById(R.id.imageview);
-//        ViewGroup.LayoutParams params = imgageView.getLayoutParams();
-//        params.width = getScreenWidth(this);
-//        params.height = px2dip(this,480);
-//        Log.i("1111", params.width + "**" + params.height);
-//        imgageView.setLayoutParams(params);
-
         CountDownTimer cdt = new CountDownTimer(10000, 100) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -108,13 +119,33 @@ public class TestActivity extends Activity {
 
             }
         };
+    }
 
-        cdt.start();
-        cdt.cancel();
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     public static int px2dip(Context context, float pxValue) {
         return (int) (pxValue *density  + 0.5f);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("onpause=====","");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("onstop=====","");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("ondestroy=====","");
     }
 
     /**
@@ -146,4 +177,15 @@ public class TestActivity extends Activity {
         wm.getDefaultDisplay().getMetrics(outMetrics);
         return outMetrics.heightPixels;
     }
+    /**
+     * 检测网络是否可用
+     *
+     * @return
+     */
+    public static boolean isNetworkConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        return ni != null && ni.isConnectedOrConnecting();
+    }
+
 }
